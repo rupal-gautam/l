e998668bc34bc3d1595983c7de6b041831b2236e
rupal-gautam <rupal.gautam51@gmail.com>
2020-06-11 13:56:28 +0530

    11th challenge

---
+++ sort colors.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int z=0,o=0,t=0;
+        if(nums.size()==1)
+        {return;}
+        for(int i=0;i<nums.size();i++)
+        {
+            if(nums[i]==0)
+            {z++;}
+            if(nums[i]==1)
+            {o++;}
+            if(nums[i]==2)
+            {t++;}
+        }
+        for(int i=0;i<z;i++)
+        {
+            nums[i]=0;
+        }
+        for(int i=z;i<z+o;i++)
+        {
+            nums[i]=1;
+            
+        }
+        for(int i=z+o;i<z+o+t;i++)
+        {
+            nums[i]=2;
+        }
+        
+    }
+};
